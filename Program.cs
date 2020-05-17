e705197007621ec031e9bc27c015fd7eb59a0020
Dmytro14 <60977979+Dmytro14@users.noreply.github.com>
2020-05-17 19:57:06 +0300

    first test

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть рядок: ");
+            string input = Console.ReadLine();
+            string[] textMass;
+            textMass = input.Split(' ');
+            Console.WriteLine("Кiлькiсть слiв:");
+            Console.WriteLine(textMass.Length);
+            string[] str = input.Split('.','!', '?');
+            int maxlen = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Найдовше речення: {0}", str[index]);
+            Console.ReadLine();
+        }
+
+        }
+}
